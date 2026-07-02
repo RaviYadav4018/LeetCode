60217dd25dbfb38e1e01d089569aa3aec7ac8ece
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-07-02 17:21:35 +0530

    Time: 0 ms (100%), Space: 34.4 MB (51.41%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findlastrightnode(TreeNode* root)
+    {  
+        if(root->right==NULL) return root;
+        return findlastrightnode(root->right);
+    }
+    TreeNode* helper(TreeNode* root)
+    {
+        if(root->left==NULL)
+        {
+            return root->right;
+        }
+        else if(root->right==NULL)
+        {
+            return root->left;
+        }
+        else
+        {
+            TreeNode* rightchild= root->right;
+            TreeNode* node=findlastrightnode(root->left);
+            node->right=rightchild;
+            return root->left;
+        }
+       
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return NULL;
+        TreeNode* head=root;
+        if(root->val==key) 
+        {
+            
+            return helper(root);
+        }
+        while(root!=NULL)
+        {
+            if(root->val<key)
+            {
+                if(root->right!=NULL&&root->right->val==key)
+                {
+                    root->right=helper(root->right);
+                    break;
+                }
+                root=root->right;
+            }
+            else if(root->val>key)
+            {
+                if(root->left!=NULL&&root->left->val==key)
+                {
+                    root->left=helper(root->left);
+                    break;
+                }
+                root=root->left;
+            }
+
+        }
+        return head;
+        
+    }
+};
