cff625c68005a71298ae799cbcaad80b93e6e1a2
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-07-05 12:25:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (80.1%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num/10==0) return num;
+        while(!(num/10==0))
+        {
+            int x=0;
+            while(num>0)
+            {
+                x+=num%10;
+                num=num/10;
+            }
+            num=x;
+
+        }
+        return num;
+    }
+};
