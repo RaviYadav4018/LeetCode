388b5e54d71397b8d5166329c28cc77791609775
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-07-07 12:42:40 +0530

    Time: 0 ms (100%), Space: 8.9 MB (1.63%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i=1;
+      
         int cnt=0;
-        while(n>0)
+         int l=1;
+         int h=n;
+        while(l<=h)
         {
-            if(i<=n) cnt++;
-            n=n-i;  
-            i++;  
+            int mid =(h-l)/2+l;
+            long long sum=1LL*mid*(mid+1)/2;
+            if(sum<=n) 
+            {
+                cnt=mid;
+                l=mid+1;
+            }
+            else
+            {
+                h=mid-1;
+            }
         }
         return cnt;
     }
