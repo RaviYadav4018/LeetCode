09cdc9e88f287b2312fca2dc7a2c267aedd84463
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-07-09 09:52:45 +0530

    Time: 32 ms (16.67%), Space: 38.8 MB (38.31%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>res(m,vector<int>(n,0));
+         vector<vector<int>>vis(m,vector<int>(n,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                    res[i][j]=0;
+
+                }
+            }
+        }
+         vector<int>delrow={0,-1,0,1};
+         vector<int>delcol={-1,0,1,0};
+        while(!q.empty())
+        {
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int dis=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                 int nrow=row+delrow[i];
+                 int ncol=col+delcol[i];
+                if(nrow>=0&&nrow<m&&ncol>=0&&ncol<n&&mat[nrow][ncol]==1&&vis[nrow][ncol]!=1)
+                {
+                    q.push({{nrow,ncol},dis+1});
+                    res[nrow][ncol]=dis+1;
+                    vis[nrow][ncol]=1;
+                }
+            }
+
+        }
+        return res;
+        
+    }
+};
