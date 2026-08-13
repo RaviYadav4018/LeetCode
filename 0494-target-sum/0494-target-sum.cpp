19993e31a294101233d3f8c7cb5d8ad7e986ad7b
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-08-13 20:34:32 +0530

    Time: 10 ms (57.25%), Space: 41.4 MB (15.29%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-int f(int ind,int currsum,int target,vector<int>& nums)
+int f(int ind,int currsum,int target,vector<int>& nums,vector<vector<int>>&dp)
 {
     if(ind==-1)
     {
         if(currsum==target) return 1;
         else return 0;
     }
-    int pos=f(ind-1,currsum+nums[ind],target,nums);
-    int neg=f(ind-1,currsum-nums[ind],target,nums);
+    if(dp[ind][currsum+1000]!=-1) return dp[ind][currsum+1000];
+    int pos=f(ind-1,currsum+nums[ind],target,nums,dp);
+    int neg=f(ind-1,currsum-nums[ind],target,nums,dp);
 
-    return pos+neg;    
+    return dp[ind][currsum+1000]=pos+neg;    
 }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int currsum=0;
-        return f(n-1,currsum,target,nums);   
+        vector<vector<int>>dp(n,vector<int>(2002,-1));
+        return f(n-1,currsum,target,nums,dp);   
     }
 };
