ed3d9dab3bbc4e5895c7d14e758363fc1243109a
RaviYadav4018 <157291221+RaviYadav4018@users.noreply.github.com>
2026-08-21 14:12:30 +0530

    Time: 34 ms (52.16%), Space: 31.8 MB (16.27%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+         for(int i=0;i<=m;i++) dp[0][i]=0;
+         for(int i=1;i<=n;i++)
+         {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else
+                {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+         }
+        return dp[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+          return longestCommonSubsequence(s,s1);
+    }
+    int minInsertions(string s) {
+        int ss=longestPalindromeSubseq(s);
+        return s.size()-ss;
+        
+    }
+};
